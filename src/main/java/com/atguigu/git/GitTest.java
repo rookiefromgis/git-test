7053628d733d7a96504f9ae2ab1dbb4998a85072
yuhan <jinyuhan@ufl.edu>
2023-03-13 19:51:46 +0800

    1951

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.print("hello git!");
         System.out.print("hello git2!");
+        System.out.print("new code changed!");
     }
 }

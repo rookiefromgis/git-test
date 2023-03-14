348e188017ca49cfc56d4456146db8609ce0ce01
yuhan <jinyuhan@ufl.edu>
2023-03-14 14:03:19 +0800

    4th commit

---
+++ src/main/java/com/atguigu/git/database.java
@@ -0,0 +1,63 @@
+package com.atguigu.git;
+
+public class database {
+    @Override
+    public String toString() {
+        return "database{" +
+                "age=" + age +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    int age;
+    String              username;
+    String              password;
+    String              gender;
+    String              address;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+
+}
